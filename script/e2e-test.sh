04d68186f939668980bcb51fd6f8364c1085a9d0
joyme123 <jiangpengfei12@gmail.com>
2020-03-09 20:04:02 +0800

    增加e2e测试

--- script/e2e-test.sh
+++ script/e2e-test.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+./deployment/webhook-create-signed-cert.sh \
+    --service lazykube-webhook-svc \
+    --secret lazykube-webhook-certs \
+    --namespace default
+
+cat deployment/mutatingwebhook.yaml | \
+    deployment/webhook-patch-ca-bundle.sh > \
+    deployment/mutatingwebhook-ca-bundle.yaml
+
+kubectl create -f deployment/deployment.yaml
+kubectl create -f deployment/mutatingwebhook-ca-bundle.yaml
+
+# 测试创建pod
+kubectl create -f e2e/gcr.io-pod.yaml
+kubectl create -f e2e/quayio-pod.yaml
+kubectl create -f e2e/k8s.gcr.io-pod.yaml
+
+# 测试kubeflow
+e2e/kubeflow/deploy.sh
